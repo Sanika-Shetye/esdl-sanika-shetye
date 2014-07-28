079c0245636da11681cb4e8270a0fee531194703
Sanika-Shetye <Sanika.Shetye@cumminscollege.in>
2014-07-28 11:05:59 +0530

    Update calc.cpp
    Modified Calculator

--- calc.cpp
+++ calc.cpp
@@ -4,5 +4,11 @@ void main()
 {
 
 cout<<"Welcome to gitHub!!";
-
+int a=5,b=6,c;
+c=a+b;
+cout<<"\nAddition is:"<<c;
+c=a-b;
+cout<<"\nSubtraction is:"<<c;
+c=a*b;
+cout<<"\nMultiplication is:"<<c;
 }
